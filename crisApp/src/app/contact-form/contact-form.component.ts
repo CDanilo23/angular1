a98d9b14cdec01ef6d03c5784e05bf9e130eb5d4
CRISTIAN DANILO ORDOÑEZ <CRISTIAN DANILO ORDOEZ@192.168.1.2>
2017-12-30 13:30:44 -0500

    primer commit proyecto formulario

---
+++ crisApp/src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent {
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+}
